e073b9e5d0f8f0f659bf75b901b077f5cf84f3db
Zied Bennani <ziedbennani@gmail.com>
2024-04-25 19:58:51 +0200

    part-2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import {
   Ecommerce,
@@ -22,10 +21,13 @@ import {
   ColorMapping,
   Editor,
 } from "./pages";
+
+import { useStateContext } from "./contexts/ContextProvider";
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
+
   return (
     <div>
       <BrowserRouter>
